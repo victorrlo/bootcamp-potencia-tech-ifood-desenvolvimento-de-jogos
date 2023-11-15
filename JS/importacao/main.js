763745defce59415c7f9f6062339a56d603e64ac
victorrlo <42052096+victorrlo@users.noreply.github.com>
2023-11-15 19:11:33 -0300

    finalização importação com js

--- JS/importacao/main.js
+++ JS/importacao/main.js
@@ -1,4 +1,17 @@
-const funcoes = require('./funcoes-auxiliares');
+// const funcoes = require('./funcoes-auxiliares');
 const {gets, print} = require('./funcoes-auxiliares');
 
-print(gets());
+// print(gets());
+
+// faça um programa que recebe 5 numeros sorteados de 1-100 e mostre o maior número
+
+let maiorValorEncontrado = 0;
+
+for(let i = 0; i<5; i++){
+    const numeroSorteado = gets();
+    if(numeroSorteado>maiorValorEncontrado){
+        maiorValorEncontrado=numeroSorteado;
+    }
+}
+
+print(maiorValorEncontrado);
